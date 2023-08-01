d7d966f0035538e2e0e636cfcf77d92992b45e13
GMWebDevelopment <gmwebdevelopments@gmail.com>
2023-08-01 17:36:03 +0200

    🔥 F01: Add Logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Comportement
+
+  const [formSubmit, setFormSubmit] = useState<string>("");
+
+  function submitForm(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    alert("Bonjour " + formSubmit);
+
+    // Je vide le formulaire
+    setFormSubmit("");
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Bienvenue chez nous </h1>
+      <h2>Connectez vous ! </h2>
+
+      <form onSubmit={submitForm} method="post">
+        <input
+          value={formSubmit}
+          onChange={(e) => setFormSubmit(e.target.value)}
+          type="text"
+          placeholder="Entrez votre prénom..."
+          required
+        />
+        <button type="submit">Accéder à votre espace</button>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
