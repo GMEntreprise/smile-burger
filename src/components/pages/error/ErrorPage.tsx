dc843ea796799f4f97f56ca9ad624084006f1986
GMWebDevelopment <gmwebdevelopments@gmail.com>
2023-08-03 16:25:41 +0200

    ✨ F02: ERROR> Add redirection from and to LOGIN

--- src/components/pages/error/ErrorPage.tsx
+++ src/components/pages/error/ErrorPage.tsx
@@ -4,9 +4,10 @@ export const ErrorPage = () => {
   return (
     <div>
       <h1>ErrorPage</h1>
-      <button>
-        <Link to="/">Retourner à la page d'accueil </Link>
-      </button>
+
+      <Link to="/">
+        <button>Retourner à la page d'accueil</button>{" "}
+      </Link>
     </div>
   );
 };
