9b94310e677d69179231b97f0011ea6dcbd36bf6
GMWebDevelopment <gmwebdevelopments@gmail.com>
2024-01-23 12:58:26 +0100

    🚧 F04: OrderPage > Layout > add container

--- src/components/pages/OrderPage.tsx
+++ src/components/pages/OrderPage.tsx
@@ -1,4 +1,5 @@
 import { Link, useParams } from "react-router-dom";
+import styled from "styled-components";
 
 export const OrderPage = () => {
   // STATE
@@ -8,11 +9,29 @@ export const OrderPage = () => {
   // COMPORTEMENT
 
   return (
-    <div>
-      <h1>Bonjour {username}</h1>
-      <Link to="/">
-        <button>Déconnexion</button>
-      </Link>
-    </div>
+    <OrderPageStyled>
+      <div className="container">
+        <h1>
+          Bonjour, <span>{username}</span>
+        </h1>
+        <Link to="/">
+          <button>Déconnexion</button>
+        </Link>
+      </div>
+    </OrderPageStyled>
   );
 };
+
+const OrderPageStyled = styled.div`
+  background: #530101;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .container {
+    background: orange;
+    height: 95vh;
+    width: 1400px;
+  }
+`;
